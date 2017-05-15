6be761db1d08f2f9c558752674769b1805434c28
shanhaizhang <zhangshanhai123@gmail.com>
2017-05-15 20:44:58 +0800

    调整样式

--- wxParse/html2json.js
+++ wxParse/html2json.js
@@ -1,4 +1,7 @@
 /**
+ * html2Json 改造来自: https://github.com/Jxck/html2json
+ * 
+ * 
  * author: Di (微信小程序开发工程师)
  * organization: WeAppDev(微信小程序开发论坛)(http://weappdev.com)
  *               垂直微信小程序开发交流社区
@@ -13,8 +16,8 @@ var __placeImgeUrlHttps = "https";
 var __emojisReg = '';
 var __emojisBaseSrc = '';
 var __emojis = {};
-var wxDiscode = require('wxDiscode.js');
-var HTMLParser = require('htmlparser.js');
+var wxDiscode = require('./wxDiscode.js');
+var HTMLParser = require('./htmlparser.js');
 // Empty Elements - HTML 5
 var empty = makeMap("area,base,basefont,br,col,frame,hr,img,input,link,meta,param,embed,command,keygen,source,track,wbr");
 // Block Elements - HTML 5
@@ -46,8 +49,8 @@ function q(v) {
 function removeDOCTYPE(html) {
     return html
         .replace(/<\?xml.*\?>\n/, '')
-        .replace(/<!doctype.*\>\n/, '')
-        .replace(/<!DOCTYPE.*\>\n/, '');
+        .replace(/<.*!doctype.*\>\n/, '')
+        .replace(/<.*!DOCTYPE.*\>\n/, '');
 }
 
 
@@ -130,7 +133,32 @@ function html2json(html, bindName) {
                 results.images.push(node);
                 results.imageUrls.push(imgUrl);
             }
+            
+            // 处理font标签样式属性
+            if (node.tag === 'font') {
+                var fontSize = ['x-small', 'small', 'medium', 'large', 'x-large', 'xx-large', '-webkit-xxx-large'];
+                var styleAttrs = {
+                    'color': 'color',
+                    'face': 'font-family',
+                    'size': 'font-size'
+                };
+                if (!node.attr.style) node.attr.style = [];
+                if (!node.styleStr) node.styleStr = '';
+                for (var key in styleAttrs) {
+                    if (node.attr[key]) {
+                        var value = key === 'size' ? fontSize[node.attr[key]-1] : node.attr[key];
+                        node.attr.style.push(styleAttrs[key]);
+                        node.attr.style.push(value);
+                        node.styleStr += styleAttrs[key] + ': ' + value + ';';
+                    }
+                }
+            }
 
+            //临时记录source资源
+            if(node.tag === 'source'){
+                results.source = node.attr.src;
+            }
+            
             if (unary) {
                 // if this tag dosen't have end tag
                 // like <img src="hoge.png"/>
@@ -150,6 +178,12 @@ function html2json(html, bindName) {
             var node = bufArray.shift();
             if (node.tag !== tag) console.error('invalid state: mismatch end tag');
 
+            //当有缓存source资源时于于video补上src资源
+            if(node.tag === 'video' && results.source){
+                node.attr.src = results.source;
+                delete result.source;
+            }
+            
             if (bufArray.length === 0) {
                 results.nodes.push(node);
             } else {
@@ -180,15 +214,15 @@ function html2json(html, bindName) {
         },
         comment: function (text) {
             //debug(text);
-            var node = {
-                node: 'comment',
-                text: text,
-            };
-            var parent = bufArray[0];
-            if (parent.nodes === undefined) {
-                parent.nodes = [];
-            }
-            parent.nodes.push(node);
+            // var node = {
+            //     node: 'comment',
+            //     text: text,
+            // };
+            // var parent = bufArray[0];
+            // if (parent.nodes === undefined) {
+            //     parent.nodes = [];
+            // }
+            // parent.nodes.push(node);
         },
     });
     return results;
